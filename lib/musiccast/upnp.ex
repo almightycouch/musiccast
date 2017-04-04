d8b46d914a9a244e7a941278821625a42b6ab416
Mario Flach <m.flach@almightycouch.org>
2017-04-04 17:53:16 +0200

    Add MusicCast.UPnP.describe/1

--- lib/musiccast/upnp.ex
+++ lib/musiccast/upnp.ex
@@ -5,11 +5,24 @@ defmodule MusicCast.UPnP do
   """
 
   @doc """
-  Calls an action on a UPNP service.
+  Returns a map representing the UPnP service.
+  """
+  @spec describe(String.t) :: {:ok, term} | {:error, term}
+  def describe(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        {:ok, deserialize_desc(body)}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Calls an action on a UPnP service.
   """
   @spec call_action(String.t, String.t, String.t | Atom.t, Map.t) :: {:ok, term} | {:error, term}
   def call_action(url, service_type, action, params) do
-    body = serialize(service_type, action, params)
+    body = serialize_action(service_type, action, params)
     case HTTPoison.post(url, body, ["SOAPAction": "\"#{service_type}##{action}\""]) do
       {:ok, %HTTPoison.Response{body: body, status_code: status}} when status in 200..299 ->
         {:ok, xpath(body, ~x"//s:Envelope/s:Body/u:#{action}Response[xmlns:u=#{service_type}]")}
@@ -27,7 +40,7 @@ defmodule MusicCast.UPnP do
   # Helpers
   #
 
-  defp serialize(service_type, action, params) do
+  defp serialize_action(service_type, action, params) do
     service_type
     |> body_params(action, params)
     |> List.wrap
@@ -36,6 +49,27 @@ defmodule MusicCast.UPnP do
     |> :xmerl.export_simple(:xmerl_xml)
     |> List.flatten
     |> to_string
+  end
+
+  defp deserialize_desc(xml) do
+    xpath(xml,
+      ~x"//*",
+      action_list: [
+        ~x"./actionList/action"l,
+        name: ~x"./name/text()"s,
+        argument_list: [
+          ~x"./argumentList/argument"l,
+          name: ~x"./name/text()"s,
+          direction: ~x"./direction/text()"s,
+          related_state_variable: ~x"./relatedStateVariable/text()"s
+        ]
+      ],
+      property_list: [
+        ~x"./serviceStateTable/stateVariable"l,
+        name: ~x"./name/text()"s,
+        type: ~x"./dataType/text()"s
+      ]
+    )
   end
 
   defp envelope(body) do
