88ced87e8b454b5bd92fc2ac4a3d1be86e370877
Mario Flach <m.flach@almightycouch.org>
2017-04-06 23:08:25 +0200

    Revert last commit
    This reverts commit 2eea9cf.

--- lib/musiccast/application.ex
+++ lib/musiccast/application.ex
@@ -10,16 +10,11 @@ defmodule MusicCast.Application do
       supervisor(MusicCast.Network, []),
       supervisor(Registry, [:duplicate, MusicCast.PubSub],   id: :pubsub),
       supervisor(Registry, [:unique,    MusicCast.Registry], id: :registry),
-      server(MusicCast.UPnP.DLNA.Endpoint, [port: 4000]),
       worker(MusicCast.UPnP.SSDPClient, []),
-      worker(MusicCast.Network.EventListener, [])
+      worker(MusicCast.Network.EventListener, []),
     ]
 
     opts = [strategy: :one_for_one, name: MusicCast.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp server(endpoint, options) do
-    Plug.Adapters.Cowboy.child_spec(:http, endpoint, [], options)
   end
 end
